2a4d0a1286f31e8e714afb761e0fd32ce19371bc
satyatejaswini1234 <127006272+satyatejaswini1234@users.noreply.github.com>
2024-07-23 08:55:03 +0530

    TestCommit

---
+++ index.js
@@ -0,0 +1,5 @@
+import ReactDOM from "react-dom/client";
+import Assign from "./assignment";
+const rootElement = document.getElementById('root');
+const rootRef = ReactDOM.createRoot(rootElement);
+rootRef.render(<Assign/>);
